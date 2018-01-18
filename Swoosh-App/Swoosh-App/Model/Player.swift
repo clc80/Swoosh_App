fd818e627f7478d8639aea70d0f46fa3fbc85d33
clc80 <clc_80@yahoo.com>
2018-01-17 21:10:40 -0800

    Added button logic on the League VC controller

---
+++ Swoosh-App/Swoosh-App/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh-App
+//
+//  Created by Claudia Contreras on 1/17/18.
+//  Copyright © 2018 thecoderpilot. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
